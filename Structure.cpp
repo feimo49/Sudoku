d26e2bfbf5f85bf63dde1c032ada01b271fccdb6
feimo49 <44833683+feimo49@users.noreply.github.com>
2018-12-21 21:59:45 +0800

    Add files via upload
    更改了数独盘写入文件的方式，从逐行写入更改为将一个终盘存入一个字符串内一次性写入文件，使在本机生成1e6个终局的时间从42s提升至4s，性能大大提升。

--- Structure.cpp
+++ Structure.cpp
@@ -36,13 +36,14 @@ void print(FILE* fp, BOARD board, int flag)
 {
 	if (flag != 0)
 		fputc('\n', fp);
-	fprintf(fp, "%d%c%d%c%d%c%d%c%d%c%d%c%d%c%d%c%d\n", board.map[0][0], ' ', board.map[0][1], ' ', board.map[0][2], ' ', board.map[0][3], ' ', board.map[0][4], ' ', board.map[0][5], ' ', board.map[0][6], ' ', board.map[0][7], ' ', board.map[0][8]);
-	fprintf(fp, "%d%c%d%c%d%c%d%c%d%c%d%c%d%c%d%c%d\n", board.map[1][0], ' ', board.map[1][1], ' ', board.map[1][2], ' ', board.map[1][3], ' ', board.map[1][4], ' ', board.map[1][5], ' ', board.map[1][6], ' ', board.map[1][7], ' ', board.map[1][8]);
-	fprintf(fp, "%d%c%d%c%d%c%d%c%d%c%d%c%d%c%d%c%d\n", board.map[2][0], ' ', board.map[2][1], ' ', board.map[2][2], ' ', board.map[2][3], ' ', board.map[2][4], ' ', board.map[2][5], ' ', board.map[2][6], ' ', board.map[2][7], ' ', board.map[2][8]);
-	fprintf(fp, "%d%c%d%c%d%c%d%c%d%c%d%c%d%c%d%c%d\n", board.map[3][0], ' ', board.map[3][1], ' ', board.map[3][2], ' ', board.map[3][3], ' ', board.map[3][4], ' ', board.map[3][5], ' ', board.map[3][6], ' ', board.map[3][7], ' ', board.map[3][8]);
-	fprintf(fp, "%d%c%d%c%d%c%d%c%d%c%d%c%d%c%d%c%d\n", board.map[4][0], ' ', board.map[4][1], ' ', board.map[4][2], ' ', board.map[4][3], ' ', board.map[4][4], ' ', board.map[4][5], ' ', board.map[4][6], ' ', board.map[4][7], ' ', board.map[4][8]);
-	fprintf(fp, "%d%c%d%c%d%c%d%c%d%c%d%c%d%c%d%c%d\n", board.map[5][0], ' ', board.map[5][1], ' ', board.map[5][2], ' ', board.map[5][3], ' ', board.map[5][4], ' ', board.map[5][5], ' ', board.map[5][6], ' ', board.map[5][7], ' ', board.map[5][8]);
-	fprintf(fp, "%d%c%d%c%d%c%d%c%d%c%d%c%d%c%d%c%d\n", board.map[6][0], ' ', board.map[6][1], ' ', board.map[6][2], ' ', board.map[6][3], ' ', board.map[6][4], ' ', board.map[6][5], ' ', board.map[6][6], ' ', board.map[6][7], ' ', board.map[6][8]);
-	fprintf(fp, "%d%c%d%c%d%c%d%c%d%c%d%c%d%c%d%c%d\n", board.map[7][0], ' ', board.map[7][1], ' ', board.map[7][2], ' ', board.map[7][3], ' ', board.map[7][4], ' ', board.map[7][5], ' ', board.map[7][6], ' ', board.map[7][7], ' ', board.map[7][8]);
-	fprintf(fp, "%d%c%d%c%d%c%d%c%d%c%d%c%d%c%d%c%d\n", board.map[8][0], ' ', board.map[8][1], ' ', board.map[8][2], ' ', board.map[8][3], ' ', board.map[8][4], ' ', board.map[8][5], ' ', board.map[8][6], ' ', board.map[8][7], ' ', board.map[8][8]);
+	char str[163];
+	for (int i = 0; i < 162; i++)
+	{
+		int t = i % 18;
+		if (t == 17)str[i] = '\n';
+		else if (i % 2 == 1)str[i] = ' ';
+		else str[i] = board.map[i / 18][t / 2];
+	}
+	str[162] = '\0';
+	fputs(str, fp);
 }
